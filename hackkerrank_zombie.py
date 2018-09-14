cf618f5652a24bae6c73b73ea5f2f7750b407f5f
Aditi <aditi.godbole01@gmail.com>
2018-09-14 13:31:43 -0500

    z problem
    z problem

--- hackkerrank_zombie.py
+++ hackkerrank_zombie.py
@@ -1,5 +1,25 @@
-4
-1100
-1110
-0110
-0001
+class Solution(object):
+    def zombiecluster(self,zombies):
+        clusternum = 0
+        self.zombies = zombies
+
+        z_count = [z.count(1) for z in zombies]
+        print 1
+
+
+        return clusternum
+
+
+def main():
+    n = 6
+    zombies = [[1,1,0,0],[1,1,1,0],[0,1,1,0],[0,0,0,1],[0,0,0,0],[1,1,0,0],[0,1,1,0]]
+    #zombies = [[1,1,0,0],[1,1,1,0],[0,1,1,0],[0,0,0,1]]
+    #zombies = [[1,1,0,0],[1,1,1,0],[0,1,1,0],[0,0,0,1]]
+    #zombies = [[1,1,0,0],[1,1,1,0],[0,1,1,0],[0,0,0,1]]
+
+    ret = Solution().zombiecluster(zombies)
+
+
+    print ret
+if __name__ == '__main__':
+    main()

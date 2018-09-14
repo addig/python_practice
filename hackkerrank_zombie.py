16a232fb4abbf3a8e72b93f72127e56351f5a539
Aditi Godbole <aditi.godbole01@gmail.com>
2018-09-14 14:58:11 -0500

    logic1

--- hackkerrank_zombie.py
+++ hackkerrank_zombie.py
@@ -2,6 +2,31 @@ class Solution(object):
     def zombiecluster(self,zombies):
         clusternum = 0
         self.zombies = zombies
+        left = False
+        right = False
+        up = False
+        down = False
+        nextcolflag = False
+        rowlen = len(zombies[0])
+        collen = len(zombies)
+        i = 0
+        j = 0
+        while i <(rowlen):
+            while j <(collen):
+                if zombies[i][j] ==1:
+                    if nextcolflag ==False:
+                        i+=1
+                    else :
+                        i-=1
+                    if nextcolflag ==True:
+                        clusternum +=1
+                        nextcolflag = False
+                        i+=2
+                        j-=1
+                else:
+                    j+=1
+                    nextcolflag = True
+
 
         z_count = [z.count(1) for z in zombies]
         print 1
